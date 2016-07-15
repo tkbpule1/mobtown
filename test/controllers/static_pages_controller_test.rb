633bb46eff2d66994fc45e74c6658049f75c0399
Tim <tkbpule1@gmail.com>
2016-07-14 21:43:20 -0400

    test titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Mobtown Offroad"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title'
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select 'title', "Help | #{@base_title}"
   end
 
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,8 +1,13 @@
-test 'full title helper' do
-    assert_equal full_title, "#{@base_title}"
-    assert_equal full_title('Help'), "Help | #{@base_title}"
-    # assert_equal full_title('About'), "About | #{@base_title}"
-    # assert_equal full_title('Contact'), "Contact | #{@base_title}"
-    # assert_equal full_title('Login'), "Login | #{@base_title}"
-    # assert_equal full_title('Signup'), "Signup | #{@base_title}"
-  end
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+
+  test 'full title helper' do
+      assert_equal full_title, "#{@base_title}"
+      assert_equal full_title("Help"), "Help | #{@base_title}"
+      # assert_equal full_title('About'), "About | #{@base_title}"
+      # assert_equal full_title('Contact'), "Contact | #{@base_title}"
+      # assert_equal full_title('Login'), "Login | #{@base_title}"
+      # assert_equal full_title('Signup'), "Signup | #{@base_title}"
+    end
+end
