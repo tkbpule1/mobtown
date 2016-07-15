a311f2699ba9104e573c01021f00491ba9bd6d1a
Tim <tkbpule1@gmail.com>
2016-07-14 22:51:32 -0400

    users signup test & error msgs partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index
-    render html: 'Mobtown Offroad Toyota Truck Products'
+  include SessionHelper
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please Log In."
+      redirect_to login_url
+    end
   end
 end
