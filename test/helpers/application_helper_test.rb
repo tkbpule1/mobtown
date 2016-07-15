05ae072ed05a5893708886593ef6980c11b1a2a1
Tim <tkbpule1@gmail.com>
2016-07-14 21:18:54 -0400

    setup tests

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+test 'full title helper' do
+    assert_equal full_title, "#{@base_title}"
+    assert_equal full_title('Help'), "Help | #{@base_title}"
+    # assert_equal full_title('About'), "About | #{@base_title}"
+    # assert_equal full_title('Contact'), "Contact | #{@base_title}"
+    # assert_equal full_title('Login'), "Login | #{@base_title}"
+    # assert_equal full_title('Signup'), "Signup | #{@base_title}"
+  end
